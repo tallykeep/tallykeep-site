822f1d45739135455326c7eb94fa564c385cea03
tallykeep <go.tallybookkeeping@gmail.com>
2025-06-23 00:21:19 +0800

    Create cookie.js

---
+++ assets/js/cookie.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.getElementById("cookieOverlay");
+  const acceptBtn = document.getElementById("cookieAcceptBtn");
+
+  if (!localStorage.getItem("cookieConsentGiven")) {
+    overlay.style.display = "flex";
+  }
+
+  acceptBtn.addEventListener("click", function () {
+    localStorage.setItem("cookieConsentGiven", "true");
+    overlay.style.display = "none";
+  });
+});
